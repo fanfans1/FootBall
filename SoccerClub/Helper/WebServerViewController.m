22db784ac085265e47519b59262ee028592f8692
fanfans1 <986103772@qq.com>
2017-09-26 17:59:42 +0800

    阿杰 你真菜

--- SoccerClub/Helper/WebServerViewController.m
+++ SoccerClub/Helper/WebServerViewController.m
@@ -23,12 +23,12 @@
     // 将请求放入子线程中
     dispatch_async(dispatch_get_global_queue(0, 0), ^{
         
-        NSURL *url = [NSURL URLWithString:@"http://www.dongqiudi.com/"];
-        NSURLRequest *request = [NSURLRequest requestWithURL:url];
-        [webView loadRequest:request];
-        
         // 回主线程
         dispatch_async(dispatch_get_main_queue(), ^{
+            
+            NSURL *url = [NSURL URLWithString:@"http://sports.163.com/world/"];
+            NSURLRequest *request = [NSURLRequest requestWithURL:url];
+            [webView loadRequest:request];
             
             webView.delegate = self;
             [self.view addSubview:webView];
@@ -43,14 +43,26 @@
 
 // 去除广告
 -(void)webViewDidFinishLoad:(UIWebView *)webView{
-    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementById('show').style.display = 'none'"];
-    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementById('top').style.display = 'none'"]; // MAC广告
-    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementById('header').style.display = 'none'"];
-    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementById('footer').style.display = 'none'"];
-    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('right')[0].style.display = 'none'"];
-    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementById('ctr').style.display = 'none'"];
-//    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByTagName('STRONG')[0].innerText = ''"];
-//    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('pos')[0].style.display = 'none'"];
+    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('ntes_nav_wrap ntes-nav-wrap-resize1024')[0].style.display = 'none'"];
+    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('N-nav-channel JS_NTES_LOG_FE')[0].style.display = 'none'"]; // MAC广告
+     [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('channel')[0].style.display = 'none'"]; // MAC广告
+     [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('area topad channel_relative_2016')[0].style.display = 'none'"]; // MAC广告
+     [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('left_part')[0].style.display = 'none'"]; // MAC广告
+     [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('right_part')[0].style.display = 'none'"]; // MAC广告
+     [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('top_news_focus')[0].style.display = 'none'"]; // MAC广告
+     [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('topnews_block')[0].style.display = 'none'"]; // MAC广告
+     [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('topnews_block')[1].style.display = 'none'"]; // MAC广告
+    
+    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('topnews_block')[2].style.display = 'none'"];
+    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('topnews_block')[3].style.display = 'none'"];
+    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('topnews_block')[4].style.display = 'none'"];
+    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('foot_execute_leader area')[0].style.display = 'none'"];
+    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('h70')[0].style.display = 'none'"];
+    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('footerbg')[0].style.display = 'none'"];
+    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('banner')[0].style.display = 'none'"];
+    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('N-nav-bottom')[0].style.display = 'none'"];
+    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('area bottomad channel_relative_2016')[0].style.display = 'none'"];
+    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('bottomnews_focus')[0].style.display = 'none'"];
 //    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('banner')[0].style.display = 'none'"];
 //    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementById('zbb_path').style.display = 'none'"];
 }
