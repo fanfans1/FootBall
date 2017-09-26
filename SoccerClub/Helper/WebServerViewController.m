18a597cbaed1419077e53b03cb762e343175a4d0
fanfans1 <986103772@qq.com>
2017-09-26 16:58:31 +0800

    commit

--- SoccerClub/Helper/WebServerViewController.m
+++ SoccerClub/Helper/WebServerViewController.m
@@ -17,27 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    UIButton *Btn = [UIButton buttonWithType:UIButtonTypeSystem];
-    Btn.frame = CGRectMake(5, 22, 60, 40);
-    [Btn setTitle:@"返回" forState:UIControlStateNormal];
-    [Btn addTarget:self action:@selector(leftBtnItem) forControlEvents:UIControlEventTouchUpInside];
-    Btn.titleLabel.font = [UIFont systemFontOfSize:20];
-    //    [self.view addSubview:Btn];
-    
-    UIView *headerView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.frame), 64)];
-    headerView.backgroundColor = BACKGROUNDCOLOR;
-    [headerView addSubview:Btn];
-    [self.view addSubview:headerView];
-    //     拼接
-    //    NSString *string = [NSString stringWithFormat:@"http://www.zhiboba.com/article/show/%@",self.str];
-    
-    //    UITapGestureRecognizer *tapGR = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapAction)];
-    //    [tapGR setNumberOfTapsRequired:2];
-    //    [tapGR setNumberOfTouchesRequired:1];
-    //    self.view.userInteractionEnabled = YES;
-    //    [self.view addGestureRecognizer:tapGR];
-    
-    
     
     // 使用web承接HTML
     UIWebView *webView = [[UIWebView alloc]initWithFrame:CGRectMake(0, 64, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame)-64)];
@@ -64,7 +43,7 @@
 
 // 去除广告
 -(void)webViewDidFinishLoad:(UIWebView *)webView{
-    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementById('header').style.display = 'none'"];
+    [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('head')[0].style.display = 'none'"];
     [webView stringByEvaluatingJavaScriptFromString:@"document.getElementById('banner').style.display = 'none'"]; // MAC广告
     [webView stringByEvaluatingJavaScriptFromString:@"document.getElementById('index_view_navigator').style.display = 'none'"];
     [webView stringByEvaluatingJavaScriptFromString:@"document.getElementById('BAIDU_EXP_MOB__wrapper_u2363177_0').style.display = 'none'"];
@@ -75,25 +54,14 @@
     [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('banner')[0].style.display = 'none'"];
     [webView stringByEvaluatingJavaScriptFromString:@"document.getElementById('zbb_path').style.display = 'none'"];
 }
--(void)leftBtnItem{
-    if (self.navigationController) {
-        [self.navigationController popViewControllerAnimated:YES];
-    }else{
-        [self dismissViewControllerAnimated:YES completion:nil];
-    }
-}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-// 创建边缘轻扫手势
--(void)screenEdgePan{
-    UIScreenEdgePanGestureRecognizer *screenEdgePan = [[UIScreenEdgePanGestureRecognizer alloc] initWithTarget:self action:@selector(leftBtnItem)];
-    screenEdgePan.edges = UIRectEdgeLeft;
-    [self.view addGestureRecognizer:screenEdgePan];
-}
+
 
 /*
  #pragma mark - Navigation
